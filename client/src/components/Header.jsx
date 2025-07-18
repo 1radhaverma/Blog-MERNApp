d697350727a23bfccb1df332f1d8a033a1fe214f
Radha Verma <radhaverma1099@gmail.com>
2025-07-18 19:57:33 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,10 +14,11 @@ export default function Header() {
   const path = useLocation().pathname;
 
   return (
-    <Navbar className='border-b-2'>
+   <Navbar className='border-b-2'>
       <Link
         to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           Disha's
         </span>
@@ -27,7 +28,7 @@ export default function Header() {
         <TextInput
           type='text'
           placeholder='Search...'
-          icon={AiOutlineSearch} // use 'icon' instead of 'rightIcon' in latest versions
+          rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
         />
       </form>
@@ -39,7 +40,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-         <Button className="bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 text-white font-medium px-4 py-2 rounded-lg">
+         <Button className="bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 text-white font-medium px-3 py-2 rounded-lg">
   Sign In
 </Button>
         </Link>
