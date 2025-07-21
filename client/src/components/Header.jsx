d95c4a968c0312c6ac9e9b34aa60a803786b7ab3
Radha Verma <radhaverma1099@gmail.com>
2025-07-22 00:53:15 +0530

    update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Button, Navbar, TextInput, NavbarToggle,  NavbarCollapse,NavbarLink} from 'flowbite-react';
+import { Button, Navbar, TextInput, NavbarToggle,  NavbarCollapse,NavbarLink, Avatar,Dropdown,DropdownItem, DropdownDivider } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
    <Navbar className='border-b-2'>
@@ -31,15 +33,34 @@ export default function Header() {
         <Button className='w-13 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-       <Button className="bg-transparent border-2 border-transparent bg-clip-text text-transparent 
-             bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 
-             hover:bg-gradient-to-r hover:from-purple-500 hover:via-indigo-500 hover:to-blue-500 
-             hover:text-grey hover:shadow-lg transition-all duration-300 
-             px-4 py-2 rounded-lg">
-  Sign In
-</Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <DropdownItem>
+              <span className='block text-sm'>@{currentUser.username}</span>
+            </DropdownItem>
+              <DropdownDivider/>
+            <Link to={'/dashboard?tab=profile'}>
+              <DropdownItem>Profile</DropdownItem>
+            </Link>
+            <DropdownDivider/>
+            <Link to={'/sign-in'}>
+            <DropdownItem>Sign out</DropdownItem>
+            </Link>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+       <Button className="bg-transparent border-2 border-transparent bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 
+          hover:bg-gradient-to-r hover:from-purple-500 hover:via-indigo-500 hover:to-blue-500 hover:text-grey hover:shadow-lg transition-all duration-300 px-4 py-2 rounded-lg"> 
+          Sign In
+       </Button>
+      </Link>
+        )}
         <NavbarToggle />
       </div>
       <NavbarCollapse>
